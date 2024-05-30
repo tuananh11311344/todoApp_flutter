856e9161fd642da19c09c7d41f26f67f2e3d0e06
Tuananh260602 <tuananh260602@gmail.com>
2024-05-31 00:40:03 +0700

    m

---
+++ lib/src/redeemPoint/redeemPoint.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Redeempoint extends StatefulWidget {
+  const Redeempoint({super.key});
+
+  @override
+  State<Redeempoint> createState() => _RedeempointState();
+}
+
+class _RedeempointState extends State<Redeempoint> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
